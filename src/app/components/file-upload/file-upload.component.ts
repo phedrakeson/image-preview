187c2a6d52dc41fef46e264582f352a21e2f7f2e
Gabriel Duarte <gabrield.developer@gmail.com>
2021-04-13 00:03:47 -0300

    feat: create fileupload methods

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-file-upload',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
+  imageURL: string;
+  uploadForm: FormGroup
+
+  constructor(private fb: FormBuilder) { 
+    this.uploadForm = this.fb.group({
+      avatar: [null],
+      name: ['']
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  showPreview(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.uploadForm.patchValue({
+      avatar: file
+    });
+    this.uploadForm.get('avatar').updateValueAndValidity();
+
+    const reader = new FileReader;
+    reader.onload = () => {
+      this.imageURL = reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
+
+  submit() {
+    console.log(this.uploadForm.value);
   }
 
 }
